81b491410024158154c08339499b4ccbc910e942
ogokdas <ogokdas@yandex.com>
2022-02-02 10:47:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 7, 3, 5, 8, 2, 9, 4, 15, 6 };
+
+            int [] sortedArray = inserSort(array);
+
+
+        }
+
+        static int[] inserSort(int[] input)
+        {
+            for (int i = 0; i < 4 - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (input[j - 1] > input[j])
+                    {
+                        int temp = input[j - 1];
+                        input[j - 1] = input[j];
+                        input[j] = temp;
+                    }
+                }
+            }
+            return input;
+        }
+
+
+
+    }
+}
